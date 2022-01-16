786d153ba3075f6de95f8c214e100cc3bda7da6d
大川貴則 <okawa.t@arsaga.jp>
2022-01-16 16:56:02 +0900

    stylelint-config-recess-order 追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,25 +3,25 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: "static",
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "arsaga_animation_ui",
+    title: 'arsaga_animation_ui',
     htmlAttrs: {
-      lang: "en",
+      lang: 'ja',
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [{ src: "~/assets/style/app.scss", lang: "scss" }],
+  css: [{ src: '~/assets/style/app.scss', lang: 'scss' }],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -32,10 +32,10 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
+    '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    "@nuxtjs/stylelint-module",
-    "@nuxtjs/composition-api/module",
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -43,4 +43,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-};
+}
--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
-    "stylelint-config-prettier",
-    "stylelint-config-recommended-scss",
-    "stylelint-rscss/config", // これの設定は生きてる？
-    "stylelint-config-prettier",
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+    'stylelint-rscss/config', // rscss命名規則
+    'stylelint-config-prettier', // コードフォーマッター　Prettierと設定がコンフリクトしない様にする設定。
+    'stylelint-config-recess-order', // プロパティの順番を決める
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {},
-};
+}
